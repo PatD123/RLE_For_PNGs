2eae8e309216c8b7dd92379a31558188197e08da
Patrick Dai <patrickdai917@gmail.com>
2024-12-26 17:15:07 -0500

    Can now write line by line each scanline of a png to a ppm.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,32 @@
 use std::fs::File;
+use std::io::Write;
 
-fn png_to_ppm(data: &[u8]) {
-    println!("{}", data.len());
-    
+fn png_to_ppm(data: &[u8]) -> Result<(), std::io::Error> {
+    let width = data.len() / 3;
+
+    let mut f = File::create("test_images/test.ppm")?;
+
+    // Writing PPM P3 header
+    let buf = ["P3\n", &width.to_string(), " 1\n", "255\n"];
+    for s in buf.iter() {
+        println!("{:?}", s.as_bytes());
+        f.write(s.as_bytes());
+    }
+
+    // Writing PPM data
+    for (i, d) in data.iter().enumerate() {
+        let mut s = d.to_string();
+        if i > 0 && i % 3 == 0 {
+            s.push_str("\n");
+        }
+        else{
+            s.push_str(" ");
+        }
+        f.write(s.as_bytes());
+    }
+
+    // End
+    Ok(())
 }
 
 fn main() {
@@ -23,9 +47,14 @@ fn main() {
 
     let res = reader.next_row();
     match res {
-        Ok(Some(r)) => png_to_ppm(r.data()),
-        _ => (),
-    }
+        Ok(Some(r)) => {
+            match png_to_ppm(r.data()) {
+                Ok(o) => println!("Converted a row of png to ppm"),
+                Err(e) => println!("{}", e),
+            }
+        },
+        _ => println!("PNG reader couldn't read a row of png."),
+    };
 
     // println!("{:?}", res.len());
 }
